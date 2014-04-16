f8868d62ebcf582be15eb490e727ccaae4921287
chinan.hjc <chinan.hjc@alibaba-inc.com>
2014-04-16 14:20:25 +0800

    线程-异常处理备注

---
+++ src/main/java/com/hjc/java_common_tools/threadException/ThreadException.java
@@ -0,0 +1,34 @@
+package com.hjc.java_common_tools.threadException;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class ThreadException {
+
+	public static void main(String[] args) {
+		Task task = new Task();
+		Thread thread = new Thread(task);
+		thread.setUncaughtExceptionHandler(new ExceptionHandler());
+		thread.start();
+	}
+
+}
+
+class Task implements Runnable {
+
+	@Override
+	public void run() {
+		int number0 = Integer.parseInt("TTT");
+	}
+}
+
+class ExceptionHandler implements UncaughtExceptionHandler {
+	public void uncaughtException(Thread t, Throwable e) {
+		System.out.printf("An exception has been captured\n");
+		System.out.printf("Thread: %s\n", t.getId());
+		System.out.printf("Exception: %s: %s\n", e.getClass().getName(),
+				e.getMessage());
+		System.out.printf("Stack Trace: \n");
+		e.printStackTrace(System.out);
+		System.out.printf("Thread status: %s\n", t.getState());
+	}
+}
